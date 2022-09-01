826b3d94d41eb76f74b3129f4615b1e7c2032e00
Alex Thomson <LXGaming@users.noreply.github.com>
2022-09-01 17:55:07 +1200

    Fix Extensions

--- LXGaming.Discord.Prompts/Utilities/Extensions.cs
+++ LXGaming.Discord.Prompts/Utilities/Extensions.cs
@@ -14,12 +14,12 @@ public static class Extensions {
 
     public static Task<IUserMessage> ModifyOriginalResponseAsync(this PromptService promptService, IInteractionContext context, CustomPrompt prompt, TimeSpan? timeout = null,
         string? text = null, Embed? embed = null, AllowedMentions? allowedMentions = null) {
-        return promptService.FollowupAsync(context.Interaction, prompt, timeout, text, embed, allowedMentions);
+        return promptService.ModifyOriginalResponseAsync(context.Interaction, prompt, timeout, text, embed, allowedMentions);
     }
 
     public static Task<IUserMessage> RespondAsync(this PromptService promptService, IInteractionContext context, CustomPrompt prompt, TimeSpan? timeout = null,
         string? text = null, Embed? embed = null, AllowedMentions? allowedMentions = null) {
-        return promptService.FollowupAsync(context.Interaction, prompt, timeout, text, embed, allowedMentions);
+        return promptService.RespondAsync(context.Interaction, prompt, timeout, text, embed, allowedMentions);
     }
 
     public static Task<IUserMessage> FollowupAsync(this PromptService promptService, IInteractionContext context, ConfirmationPrompt prompt, TimeSpan? timeout = null,
@@ -29,12 +29,12 @@ public static class Extensions {
 
     public static Task<IUserMessage> ModifyOriginalResponseAsync(this PromptService promptService, IInteractionContext context, ConfirmationPrompt prompt, TimeSpan? timeout = null,
         string? text = null, Embed? embed = null, AllowedMentions? allowedMentions = null) {
-        return promptService.FollowupAsync(context.Interaction, prompt, timeout, text, embed, allowedMentions);
+        return promptService.ModifyOriginalResponseAsync(context.Interaction, prompt, timeout, text, embed, allowedMentions);
     }
 
     public static Task<IUserMessage> RespondAsync(this PromptService promptService, IInteractionContext context, ConfirmationPrompt prompt, TimeSpan? timeout = null,
         string? text = null, Embed? embed = null, AllowedMentions? allowedMentions = null) {
-        return promptService.FollowupAsync(context.Interaction, prompt, timeout, text, embed, allowedMentions);
+        return promptService.RespondAsync(context.Interaction, prompt, timeout, text, embed, allowedMentions);
     }
 
     public static Task<IUserMessage> FollowupAsync(this PromptService promptService, IInteractionContext context, PaginationPromptBase prompt, TimeSpan? timeout = null) {
@@ -42,10 +42,10 @@ public static class Extensions {
     }
 
     public static Task<IUserMessage> ModifyOriginalResponseAsync(this PromptService promptService, IInteractionContext context, PaginationPromptBase prompt, TimeSpan? timeout = null) {
-        return promptService.FollowupAsync(context.Interaction, prompt, timeout);
+        return promptService.ModifyOriginalResponseAsync(context.Interaction, prompt, timeout);
     }
 
     public static Task<IUserMessage> RespondAsync(this PromptService promptService, IInteractionContext context, PaginationPromptBase prompt, TimeSpan? timeout = null) {
-        return promptService.FollowupAsync(context.Interaction, prompt, timeout);
+        return promptService.RespondAsync(context.Interaction, prompt, timeout);
     }
 }
